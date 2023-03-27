336e90d67d02d32fd46109276e1e597c7ba1bef3
krtodeatho <joshuikrish@gmail.com>
2023-03-27 16:32:17 +0545

    added

--- script.js
+++ script.js
@@ -3,71 +3,35 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      colors: [],
+      images: [],
       message:'',
+      input:'',
       showSuccessAlert: false,
     };
   },
   methods: {
-    calculate() {
-      this.colors = []
-      this.showSuccessAlert=false
-      const usedColors = new Set() // Keep track of used colors
-      while (this.colors.length < 6) {
-        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        let hexColor = '#' + randomColor
-        if (!usedColors.has(hexColor)) { // Check if color has already been added
-          this.colors.push(hexColor)
-          usedColors.add(hexColor)
-        }
-      }
-    },    
-    handleSpacebar(event) {
-      if (event.code === 'Space') {
-        // Your code to execute when the spacebar is pressed
-        this.calculate()
-      }
-    },
-    handleCopy(color){
-      if (navigator.clipboard) {
-        navigator.clipboard.writeText(color);
-        this.message='copied'+color
-        this.showSuccessAlert=true
-      } else {
-        var textarea = document.createElement('textarea');
-        textarea.value = color;
-        textarea.style.position = 'fixed';
-        textarea.style.opacity = 0;
-        document.body.appendChild(textarea);
-        textarea.focus();
-        textarea.select();
-        try {
-          document.execCommand('copy');
-          this.message='copied'+color
-          this.showSuccessAlert=true
-        } catch (err) {
-          console.error('Could not copy text: ', err);
-        }
-        document.body.removeChild(textarea);
-      }
+     async onEnter() {
+      const response = await axios.get(
+      "https://api.unsplash.com/search/photos?page=1&query="+this.input+"&orientation=landscape&collections=1451037&content_filter=high&aspect_ratio=1.5:1",{
+        headers: { Authorization: "Client-ID Yme6ZcumIXpWryQ0DPc249CE0ua2Mxh66Y-4W2gPAAc", },
+      }).then(response=>{
+        this.images=response.data.results
+      })
+      // alert(this.input)
       
-    },
-    handleClick(event) {
-      // Check if the clicked element is a link
-      if (event.target.tagName !== 'A' && !event.target.closest('.colored')) {
-        // Your code to execute when a non-link element is clicked goes here
-       this.showSuccessAlert=false
-      }
-    }
+      
+      
+     
+    },    
+
+  
+  
   },
   mounted() {
-    document.addEventListener('click', this.handleClick);
-    this.calculate();
-    document.addEventListener('keydown', this.handleSpacebar);
+    
   },
   beforeUnmount() {
-    document.removeEventListener('click', this.handleClick);
-    document.removeEventListener('keydown', this.handleSpacebar);
+    
   },
 
 }).mount("#app");
